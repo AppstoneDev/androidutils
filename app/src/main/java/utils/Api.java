c208612992c1132187e6813c853cb1d1ae71ab37
Manabendu Bardhan <manabendu.appstone@gmail.com>
2019-09-17 14:25:31 +0530

    //Header req added

--- app/src/main/java/utils/Api.java
+++ app/src/main/java/utils/Api.java
@@ -5,16 +5,14 @@ import java.util.HashMap;
 import io.reactivex.Observable;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.HeaderMap;
 import retrofit2.http.POST;
 import retrofit2.http.Url;
 
 public interface Api {
 
-
+    
     @FormUrlEncoded
     @POST
-    Observable<String> getReponse(@Url String url, @FieldMap HashMap<String, Object> params);
-
-
-    String loginTag = "login";
+    Observable<String> getReponse(@Url String url, @HeaderMap HashMap<String, String> headers, @FieldMap HashMap<String, Object> params);
 }

e5c3ea06cdd7fed2391b5828213aaad51e82baf3
Saraschandraa Muralisekar <saraschandraa.naidu@hotmail.com>
2020-02-23 11:46:02 +0530

    Added Method to check if date is before the current date

--- app/src/main/java/utils/DateUtils.java
+++ app/src/main/java/utils/DateUtils.java
@@ -224,4 +224,22 @@ public class DateUtils {
         return formatedDate;
     }
 
+    public static boolean checkDateIsPreviousForServerTimeStamp(String date) {
+        boolean isPrevious = false;
+        DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault());
+        DateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+
+        try {
+            Date toCheckDate = outputFormat.parse(date);
+            Date currentDate = outputFormat.parse(Calendar.getInstance().getTime().toString());
+            isPrevious = toCheckDate.before(currentDate);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+        return isPrevious;
+    }
+
 }

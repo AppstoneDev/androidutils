b3d2dd0f2dd23fec281cd9fdf7dc2a615a29327c
Saraschandraa Muralisekar <saraschandraa.naidu@hotmail.com>
2020-04-06 15:27:59 +0530

    Photo image issue fixed

--- app/src/main/java/utils/PhotoImageGrid.java
+++ app/src/main/java/utils/PhotoImageGrid.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import androidx.core.content.res.ResourcesCompat;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.transition.Transition;
 
@@ -32,121 +33,126 @@ public class PhotoImageGrid {
         int margin10 = Dimensions.dpToPx(10, context);
         int margin5 = Dimensions.dpToPx(5, context);
 
-        if (size == 1) {
-            Glide.with(context.getApplicationContext())
-                    .asBitmap()
-                    .load(images.get(0))
-                    .into(new SimpleTarget<Bitmap>() {
-                        @Override
-                        public void onResourceReady(Bitmap bitmap, Transition<? super Bitmap> transition) {
-                            int w = bitmap.getWidth();
-                            int singleImageHeight = bitmap.getHeight();
 
-                            LinearLayout singleLayout = new LinearLayout(context);
-                            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, singleImageHeight);
-                            params.setMargins(margin10, margin10, margin10, margin10);
-                            singleLayout.setLayoutParams(params);
-                            singleLayout.setBackgroundColor(ResourcesCompat.getColor(context.getResources(), R.color.colorBlack, null));
+        if (mainView != null) {
+            mainView.removeAllViews();
+            if (size == 1) {
+                Glide.with(context.getApplicationContext())
+                        .asBitmap()
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .load(images.get(0))
+                        .into(new SimpleTarget<Bitmap>() {
+                            @Override
+                            public void onResourceReady(Bitmap bitmap, Transition<? super Bitmap> transition) {
+                                int w = bitmap.getWidth();
+                                int singleImageHeight = bitmap.getHeight();
 
-                            View singleView = LayoutInflater.from(context).inflate(R.layout.cell_blog_image, null);
-                            ImageView mIvImage = singleView.findViewById(R.id.mImageView);
-                            mIvImage.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-                            Glide.with(context).load(images.get(0)).into(mIvImage);
+                                LinearLayout singleLayout = new LinearLayout(context);
+                                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, singleImageHeight);
+                                params.setMargins(margin10, margin10, margin10, margin10);
+                                singleLayout.setLayoutParams(params);
+                                singleLayout.setBackgroundColor(ResourcesCompat.getColor(context.getResources(), R.color.colorBlack, null));
 
-                            singleLayout.addView(singleView);
-                            mainView.addView(singleLayout);
-                        }
-                    });
-        } else if (size == 2) {
-            LinearLayout firstLayout = new LinearLayout(context);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, screenWidth);
-            params.weight = 0.5f;
-            params.setMargins(margin10, margin10, margin5, margin10);
-            firstLayout.setLayoutParams(params);
-            firstLayout.setBackgroundColor(ResourcesCompat.getColor(context.getResources(), R.color.colorBlack, null));
+                                View singleView = LayoutInflater.from(context).inflate(R.layout.cell_blog_image, null);
+                                ImageView mIvImage = singleView.findViewById(R.id.mImageView);
+                                mIvImage.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+                                Glide.with(context).load(images.get(0)).into(mIvImage);
 
-
-            LinearLayout secondLayout = new LinearLayout(context);
-            LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(0, screenWidth);
-            params2.weight = 0.5f;
-            params2.setMargins(margin5, margin10, margin10, margin10);
-            secondLayout.setLayoutParams(params2);
-            secondLayout.setBackgroundColor(ResourcesCompat.getColor(context.getResources(), R.color.colorBlack, null));
-
-            View view = LayoutInflater.from(context).inflate(R.layout.cell_blog_image, null);
-            ImageView mIvImage = view.findViewById(R.id.mImageView);
-            Glide.with(context).load(images.get(0)).into(mIvImage);
-            firstLayout.addView(view);
+                                singleLayout.addView(singleView);
+                                mainView.addView(singleLayout);
+                            }
+                        });
+            } else if (size == 2) {
+                LinearLayout firstLayout = new LinearLayout(context);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, screenWidth);
+                params.weight = 0.5f;
+                params.setMargins(margin10, margin10, margin5, margin10);
+                firstLayout.setLayoutParams(params);
+                firstLayout.setBackgroundColor(ResourcesCompat.getColor(context.getResources(), R.color.colorBlack, null));
 
 
-            View view2 = LayoutInflater.from(context).inflate(R.layout.cell_blog_image, null);
-            ImageView mIvImage2 = view2.findViewById(R.id.mImageView);
-            Glide.with(context).load(images.get(1)).into(mIvImage2);
-            secondLayout.addView(view2);
+                LinearLayout secondLayout = new LinearLayout(context);
+                LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(0, screenWidth);
+                params2.weight = 0.5f;
+                params2.setMargins(margin5, margin10, margin10, margin10);
+                secondLayout.setLayoutParams(params2);
+                secondLayout.setBackgroundColor(ResourcesCompat.getColor(context.getResources(), R.color.colorBlack, null));
 
-            mainView.addView(firstLayout);
-            mainView.addView(secondLayout);
-        } else {
-
-            int splitHeight = (int) screenWidth / 2;
-
-            LinearLayout firstLayout = new LinearLayout(context);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, screenWidth);
-            params.weight = 0.5f;
-            params.setMargins(margin10, margin10, margin5, margin10);
-            firstLayout.setLayoutParams(params);
-            firstLayout.setBackgroundColor(ResourcesCompat.getColor(context.getResources(), R.color.colorBlack, null));
-
-            LinearLayout secondLayout = new LinearLayout(context);
-            LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(0, screenWidth);
-            params2.weight = 0.5f;
-            params2.setMargins(margin5, margin10, margin10, margin10);
-            secondLayout.setLayoutParams(params2);
-            secondLayout.setOrientation(LinearLayout.VERTICAL);
-
-            LinearLayout firstChildLayout = new LinearLayout(context);
-            LinearLayout.LayoutParams params3 = new LinearLayout.LayoutParams(splitHeight, 0);
-            params3.weight = 0.5f;
-            firstChildLayout.setLayoutParams(params3);
-            firstChildLayout.setBackgroundColor(ResourcesCompat.getColor(context.getResources(), R.color.colorBlack, null));
-
-            LinearLayout secondChildLayout = new LinearLayout(context);
-            LinearLayout.LayoutParams params4 = new LinearLayout.LayoutParams(splitHeight, 0);
-            params4.weight = 0.5f;
-            params4.setMargins(5, 5, 5, 5);
-            secondChildLayout.setLayoutParams(params4);
-            secondChildLayout.setBackgroundColor(ResourcesCompat.getColor(context.getResources(), R.color.colorBlack, null));
+                View view = LayoutInflater.from(context).inflate(R.layout.cell_blog_image, null);
+                ImageView mIvImage = view.findViewById(R.id.mImageView);
+                Glide.with(context).load(images.get(0)).into(mIvImage);
+                firstLayout.addView(view);
 
 
-            View view = LayoutInflater.from(context).inflate(R.layout.cell_blog_image, null);
-            ImageView mIvImage = view.findViewById(R.id.mImageView);
-            Glide.with(context).load(images.get(0)).into(mIvImage);
-            firstLayout.addView(view);
+                View view2 = LayoutInflater.from(context).inflate(R.layout.cell_blog_image, null);
+                ImageView mIvImage2 = view2.findViewById(R.id.mImageView);
+                Glide.with(context).load(images.get(1)).into(mIvImage2);
+                secondLayout.addView(view2);
 
-            View view2 = LayoutInflater.from(context).inflate(R.layout.cell_blog_image, null);
-            ImageView mIvImage2 = view2.findViewById(R.id.mImageView);
-            Glide.with(context).load(images.get(1)).into(mIvImage2);
-            firstChildLayout.addView(view2);
+                mainView.addView(firstLayout);
+                mainView.addView(secondLayout);
+            } else {
 
-            View view3 = LayoutInflater.from(context).inflate(R.layout.cell_blog_image, null);
-            ImageView mIvImage3 = view3.findViewById(R.id.mImageView);
-            Glide.with(context).load(images.get(2)).into(mIvImage3);
-            if (size > 3) {
-                RelativeLayout mRlAlpha = view3.findViewById(R.id.rl_img_alpha);
-                TextView mTvCount = view3.findViewById(R.id.tvCount);
+                int splitHeight = (int) screenWidth / 2;
 
-                mRlAlpha.setVisibility(View.VISIBLE);
-                mTvCount.setVisibility(View.VISIBLE);
+                LinearLayout firstLayout = new LinearLayout(context);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, screenWidth);
+                params.weight = 0.5f;
+                params.setMargins(margin10, margin10, margin5, margin10);
+                firstLayout.setLayoutParams(params);
+                firstLayout.setBackgroundColor(ResourcesCompat.getColor(context.getResources(), R.color.colorBlack, null));
 
-                mTvCount.setText(String.valueOf(size - 3).concat(" +"));
+                LinearLayout secondLayout = new LinearLayout(context);
+                LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(0, screenWidth);
+                params2.weight = 0.5f;
+                params2.setMargins(margin5, margin10, margin10, margin10);
+                secondLayout.setLayoutParams(params2);
+                secondLayout.setOrientation(LinearLayout.VERTICAL);
+
+                LinearLayout firstChildLayout = new LinearLayout(context);
+                LinearLayout.LayoutParams params3 = new LinearLayout.LayoutParams(splitHeight, 0);
+                params3.weight = 0.5f;
+                firstChildLayout.setLayoutParams(params3);
+                firstChildLayout.setBackgroundColor(ResourcesCompat.getColor(context.getResources(), R.color.colorBlack, null));
+
+                LinearLayout secondChildLayout = new LinearLayout(context);
+                LinearLayout.LayoutParams params4 = new LinearLayout.LayoutParams(splitHeight, 0);
+                params4.weight = 0.5f;
+                params4.setMargins(5, 5, 5, 5);
+                secondChildLayout.setLayoutParams(params4);
+                secondChildLayout.setBackgroundColor(ResourcesCompat.getColor(context.getResources(), R.color.colorBlack, null));
+
+
+                View view = LayoutInflater.from(context).inflate(R.layout.cell_blog_image, null);
+                ImageView mIvImage = view.findViewById(R.id.mImageView);
+                Glide.with(context).load(images.get(0)).into(mIvImage);
+                firstLayout.addView(view);
+
+                View view2 = LayoutInflater.from(context).inflate(R.layout.cell_blog_image, null);
+                ImageView mIvImage2 = view2.findViewById(R.id.mImageView);
+                Glide.with(context).load(images.get(1)).into(mIvImage2);
+                firstChildLayout.addView(view2);
+
+                View view3 = LayoutInflater.from(context).inflate(R.layout.cell_blog_image, null);
+                ImageView mIvImage3 = view3.findViewById(R.id.mImageView);
+                Glide.with(context).load(images.get(2)).into(mIvImage3);
+                if (size > 3) {
+                    RelativeLayout mRlAlpha = view3.findViewById(R.id.rl_img_alpha);
+                    TextView mTvCount = view3.findViewById(R.id.tvCount);
+
+                    mRlAlpha.setVisibility(View.VISIBLE);
+                    mTvCount.setVisibility(View.VISIBLE);
+
+                    mTvCount.setText(String.valueOf(size - 3).concat(" +"));
+                }
+
+                secondChildLayout.addView(view3);
+                secondLayout.addView(firstChildLayout);
+                secondLayout.addView(secondChildLayout);
+
+                mainView.addView(firstLayout);
+                mainView.addView(secondLayout);
             }
-
-            secondChildLayout.addView(view3);
-            secondLayout.addView(firstChildLayout);
-            secondLayout.addView(secondChildLayout);
-
-            mainView.addView(firstLayout);
-            mainView.addView(secondLayout);
         }
 
     }

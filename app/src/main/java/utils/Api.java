9db3e9affa43d3c922714e1b927f224164eb3c5a
Saraschandraa Muralisekar <saraschandraa.naidu@hotmail.com>
2021-02-25 13:39:09 +0530

    API Methods added

--- app/src/main/java/utils/Api.java
+++ app/src/main/java/utils/Api.java
@@ -1,12 +1,18 @@
 package utils;
 
+import androidx.annotation.RawRes;
+
 import java.util.HashMap;
 
 import io.reactivex.Observable;
+import retrofit2.http.DELETE;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.HeaderMap;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.QueryMap;
 import retrofit2.http.Url;
 
 public interface Api {
@@ -15,4 +21,18 @@ public interface Api {
     @FormUrlEncoded
     @POST
     Observable<String> getReponse(@Url String url, @HeaderMap HashMap<String, String> headers, @FieldMap HashMap<String, Object> params);
+
+
+    @GET
+    Observable<String> performGetMethodRAW(@Url String url, @HeaderMap HashMap<String, String> headers, @QueryMap HashMap<String, Object> queries);
+
+    @POST
+    Observable<String> performPostMethodRAW(@Url String url, @HeaderMap HashMap<String, String> headers, @FieldMap HashMap<String, Object> params);
+
+    @PUT
+    Observable<String> performPutMethodRAW(@Url String url, @HeaderMap HashMap<String, String> headers, @FieldMap HashMap<String, Object> params);
+
+    @DELETE
+    Observable<String> performDeleteMethodRAW(@Url String url, @HeaderMap HashMap<String, String> headers, @FieldMap HashMap<String, Object> params);
+
 }
